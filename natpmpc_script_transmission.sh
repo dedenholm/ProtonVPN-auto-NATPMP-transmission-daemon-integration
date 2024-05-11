9fa58bf6772198e6f87f0407f8e5fed14bb7bcd7
Daniel Rognskog Edenholm <dedenholm@gmail.com>
2024-05-11 10:59:03 +0200

    fixed mistake where i removed writing to temp folder:

--- natpmpc_script_transmission.sh
+++ natpmpc_script_transmission.sh
@@ -4,7 +4,7 @@ while true; do
     date
      
     # Run natpmpc for UDP and TCP, redirect output to a temporary file
-    natpmpc -a 1 0 udp 60 -g 10.2.0.1 && natpmpc -a 1 0 tcp 60 -g 10.2.0.1  || { 
+    natpmpc -a 1 0 udp 60 -g 10.2.0.1 && natpmpc -a 1 0 tcp 60 -g 10.2.0.1 > /tmp/natpmpc_output || { 
         echo -e "ERROR with natpmpc command \a"
         break
     }
